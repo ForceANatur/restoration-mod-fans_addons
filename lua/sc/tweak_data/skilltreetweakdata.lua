adef64cfa190cc5faf8b5b5954a859579c3edc92
Force A Nature Scout <thomasher528@gmail.com>
2025-04-22 18:52:14 -0700

    op missed one

--- lua/sc/tweak_data/skilltreetweakdata.lua
+++ lua/sc/tweak_data/skilltreetweakdata.lua
@@ -1635,7 +1635,8 @@ function SkillTreeTweakData:init(tweak_data)
 				},
 				[2] = {
 					upgrades = {
-						"pistol_swap_speed_multiplier_1"
+						"pistol_swap_speed_multiplier_1",
+						"akimbo_swap_speed_multiplier_1"
 					},
 					cost = self.costs.pro
 				}
@@ -1648,14 +1649,16 @@ function SkillTreeTweakData:init(tweak_data)
 				["icon_xy"] = {11, 0},
 				[1] = {
 					upgrades = {
-						"pistol_hip_fire_spread_multiplier"
+						"pistol_hip_fire_spread_multiplier",
+						"akimbo_hip_fire_spread_multiplier"
 					},
 					cost = self.costs.hightier
 				},
 				[2] = {
 					upgrades = {
 						"pistol_fire_rate_multiplier",
-						"pistol_ap_bullets_1"
+						"pistol_ap_bullets_1",
+						"akimbo_ap_bullets_1"
 					},
 					cost = self.costs.hightierpro
 				}
@@ -2591,6 +2594,7 @@ function SkillTreeTweakData:init(tweak_data)
 			{
 				upgrades = {
 					"player_melee_kill_life_leech",
+					"player_melee_kill_stamina",
 					"player_corpse_dispose_amount_2"
 				},
 				cost = 1000,
