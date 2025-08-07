33ba17aedfdfe4fa942375b4f3829e388ec02c15
Force A Nature Scout <thomasher528@gmail.com>
2025-08-07 10:14:43 -0700

    Update charactertweakdata.lua

--- lua/sc/tweak_data/charactertweakdata.lua
+++ lua/sc/tweak_data/charactertweakdata.lua
@@ -1319,7 +1319,7 @@ function CharacterTweakData:_init_city_swat(presets)
 	if self:get_ai_group_type() == "russia" then
 		self.weekend_lmg.custom_voicework = "bravo_elite_ru"
 	elseif self:get_ai_group_type() == "murkywater" then
-		self.weekend_lmg.custom_voicework = "bravo_elite_murky"	
+		self.weekend_lmg.custom_voicework = "bravo_elite_murky"
 	elseif self:get_ai_group_type() == "federales" then
 		self.weekend_lmg.custom_voicework = "bravo_elite_mex"
 	else
@@ -1340,7 +1340,7 @@ function CharacterTweakData:_init_city_swat(presets)
 	table.insert(self._enemy_list, "weekend_lmg")
 	
 	--Weekend LMG (Guard Variant)
-	self.weekend_elite_guard = deep_clone(self.weekend_lmg)	
+	self.weekend_elite_guard = deep_clone(self.weekend_lmg)
 	self.weekend_elite_guard.tags = {"law", "city_swat"}
 	self.weekend_elite_guard.dt_suppress = nil
 	self.weekend_elite_guard.silent_priority_shout = "f37"
@@ -4265,6 +4265,7 @@ function CharacterTweakData:_init_city_swat_rpg(presets)
 	self.city_swat_rpg = deep_clone(self.city_swat_titan)
 	self.city_swat_rpg.dodge = presets.dodge.poor
 	self.city_swat_rpg.move_speed = presets.move_speed.slow_plus
+	self.city_swat_rpg.dodge = presets.dodge.poor
 	self.city_swat_rpg.yellow_blood = false
 	self.city_swat_rpg.HEALTH_INIT = 20
 	self.city_swat_rpg.headshot_dmg_mul = 1.9
