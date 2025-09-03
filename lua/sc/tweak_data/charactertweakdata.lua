c3c3b47af1aa6b89a9938f5b7dc93e6d6e290268
akiko-npriko <hiddenshadows353@gmail.com>
2025-09-03 18:04:12 -0500

    Update charactertweakdata.lua

--- lua/sc/tweak_data/charactertweakdata.lua
+++ lua/sc/tweak_data/charactertweakdata.lua
@@ -19617,7 +19617,7 @@ function CharacterTweakData:character_map()
 		
 		--Other
 		table.insert(char_map.basic.list, "ene_bulldozer_2_hw")		
-		table.insert(char_map.basic.list, "ene_vip_2")		
+		table.insert(char_map.basic.list, "ene_bulldozer_4_minion")		
 		table.insert(char_map.basic.list, "ene_mememan_1")
 		table.insert(char_map.basic.list, "ene_mememan_2")
 		table.insert(char_map.basic.list, "ene_bulldozer_biker_1")
@@ -20176,6 +20176,7 @@ function CharacterTweakData:character_map()
 				"ene_akan_medic_zdann",	
 				"ene_akan_lpf",
 				"ene_vip_2",
+				"ene_vip_2_assault",
 				"ene_titan_shotgun",
 				"ene_titan_rifle",
 				"ene_fbi_titan_1",
