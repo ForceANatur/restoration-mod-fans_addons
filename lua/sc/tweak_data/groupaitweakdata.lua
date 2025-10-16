dae92b1a87244e089d06c52fa5f260605a39cff1
Force A Nature Scout <thomasher528@gmail.com>
2025-10-16 12:00:20 -0700

    wait wha what the fuck how did a miss a spot

--- lua/sc/tweak_data/groupaitweakdata.lua
+++ lua/sc/tweak_data/groupaitweakdata.lua
@@ -9664,7 +9664,7 @@ function GroupAITweakData:_init_unit_categories(difficulty_index)
 				},
 				zombie = {
 					Idstring("units/pd2_mod_omnia/characters/ene_bulldozer_minigun/ene_bulldozer_minigun"),
-					Idstring("units/pd2_dlc_drm/characters/ene_bulldozer_minigun/ene_bulldozer_minigun")
+					Idstring("units/pd2_dlc_hvh/characters/ene_bulldozer_minigun/ene_bulldozer_minigun")
 				},					
 				russia = {
 					Idstring("units/pd2_mod_omnia/characters/ene_bulldozer_minigun/ene_bulldozer_minigun"),
